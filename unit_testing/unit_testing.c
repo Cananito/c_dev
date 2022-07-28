5e6fb1ce30506e21db2cff87e1048ec840853440
Rogelio Gudino <cananito@gmail.com>
2022-07-28 16:21:31 -0700

    Gets rid of NULL check in unit test assert, printf handles it well.

--- unit_testing/unit_testing.c
+++ unit_testing/unit_testing.c
@@ -5,12 +5,8 @@
 
 void assert_true(int condition_result, char* failure_message) {
   if (condition_result != 1) {
-    if (failure_message == NULL) {
-      printf("Assert failed! Specify `failure_message` for better diagnostics."
-             "\n");
-    } else {
-      printf("%s\n", failure_message);
-    }
+    printf("%s\n", failure_message);
+    printf("Tests failed.\n");
     exit(1);
   }
 }
