66b024ba16cccdb6f433dbb84ef48ce9b9943380
Rogelio Gudino <cananito@gmail.com>
2022-09-27 07:52:14 -0700

    Finishes struct memory layout sample.

--- samples/structs/structs.c
+++ samples/structs/structs.c
@@ -19,19 +19,43 @@ void PassByReference(struct MyStruct* pbr) {
 }
 
 void DemonstrateMemoryLayout(void) {
-  struct Something {
-    char first;
-    float second;
-    double third;
-  } s = { .first = 'a', .second = 11111.11111, .third = 11111111111111.11111111111111 };
-  // TODO: Not the values I was expecting. Figure out why.
-  printf("Address of s: %p\n", &s);
-  printf("Address of s.first: %p\n", &(s.first));
-  printf("Address of s.second: %p\n", &(s.second));
-  printf("Address of s.third: %p\n", &(s.third));
-  printf("Value of s.first: %c\n", s.first);
-  printf("Value of s.second: %f\n", s.second);
-  printf("Value of s.third: %lf\n", s.third);
+  // NOTE: Sizes based on 64-bit CPU.
+
+  struct PaddedStruct { // Total: 24 bytes.
+    char first; // 1 byte.
+    // 3 bytes padding.
+    double second; // 8 bytes.
+    float third; // 4 bytes.
+    // 4 bytes padding.
+  } ps = { .first = 'a', .second = 1.1, .third = 1.1 };
+  printf("Size of ps: %lu\n", sizeof(ps));
+  printf("Address of ps: %p\n", &ps);
+  printf("Address of ps.first: %p\n", &(ps.first));
+  printf("Address of ps.second: %p\n", &(ps.second));
+  printf("Address of ps.third: %p\n", &(ps.third));
+  printf("Value of ps.first: %c\n", ps.first);
+  printf("Value of ps.second: %lf\n", ps.second);
+  printf("Value of ps.third: %f\n", ps.third);
+
+  struct NoPaddingStruct { // Total: 8 bytes.
+    char first; // 1 byte.
+    char second; // 1 byte.
+    char third; // 1 byte.
+    char fourth; // 1 byte.
+    float fifth; // 4 bytes.
+  } nps = { .first = 'a', .second = 'b', .third = 'c', .fourth = 'd', .fifth = 1.1 };
+  printf("Size of nps: %lu\n", sizeof(nps));
+  printf("Address of nps: %p\n", &nps);
+  printf("Address of nps.first: %p\n", &(nps.first));
+  printf("Address of nps.second: %p\n", &(nps.second));
+  printf("Address of nps.third: %p\n", &(nps.third));
+  printf("Address of nps.fourth: %p\n", &(nps.fourth));
+  printf("Address of nps.fifth: %p\n", &(nps.fifth));
+  printf("Value of nps.first: %c\n", nps.first);
+  printf("Value of nps.second: %c\n", nps.second);
+  printf("Value of nps.third: %c\n", nps.third);
+  printf("Value of nps.fourth: %c\n", nps.fourth);
+  printf("Value of nps.fifth: %f\n", nps.fifth);
 }
 
 int main(void) {
