613bc1bd7c107f38ab7a61cfc056cdcdebe8566c
Rogelio Gudino <cananito@gmail.com>
2022-12-23 15:47:29 -0800

    Finished sample of structs.

--- samples/immutable_struct/immutable_struct.c
+++ samples/immutable_struct/immutable_struct.c
@@ -1,48 +1,14 @@
-#include <stdbool.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-
-// TODO: Move things into their own files.
 
 typedef struct Person Person;
 struct Person {
-  int8_t const age;
-  char const* const name;
-  // TODO: Convert these 2 into a List_r.
-  int8_t const* favoriteNumbers;
-  size_t const favoriteNumbersCount;
+  int8_t age;
+  char const* name;
+  int8_t favoriteNumbers[3];
+  size_t favoriteNumbersCount;
 };
-
-static Person Person_Create(int8_t age, char* name, int8_t* favoriteNumbers, size_t favoriteNumbersCount) {
-  // TODO: When is this supposed to be freed?
-  //       Probably need a Destroy function that does it?
-  int8_t* favoriteNumbersCopy = calloc(favoriteNumbersCount, sizeof(int8_t));
-  for (size_t i = 0; i < favoriteNumbersCount; i++) {
-    favoriteNumbersCopy[i] = favoriteNumbers[i];
-  }
-  return (Person){
-    .age = age,
-    .name = name,
-    .favoriteNumbers = favoriteNumbersCopy,
-    .favoriteNumbersCount = favoriteNumbersCount,
-  };
-}
-
-static void Person_Destroy(Person* person) {
-  // TODO: Copy favoriteNumbers in Update functions, use a destroyed bool pointer, or a List_r pointer?
-  // free(person->favoriteNumbers);
-}
-
-static Person Person_UpdateAge(Person person, int8_t age) {
-  return (Person){
-    .age = age,
-    .name = person.name,
-    .favoriteNumbers = person.favoriteNumbers,
-    .favoriteNumbersCount = person.favoriteNumbersCount,
-  };
-}
 
 static void Person_Print(Person const* person) {
   printf("Person (%p) { .age = %d, .name = %s, .favoriteNumbers = ",
@@ -71,18 +37,21 @@ static void Person_Print(Person const* person) {
 }
 
 int main () {
-  int8_t* rogelioFavoriteNumbers = calloc(2, sizeof(int8_t));
-  rogelioFavoriteNumbers[0] = 1;
-  rogelioFavoriteNumbers[1] = 7;
-  Person rogelio = Person_Create(33, "Rogelio", rogelioFavoriteNumbers, 2);
-  free(rogelioFavoriteNumbers);
+  Person rogelio = { .age = 33, .name = "Rogelio", .favoriteNumbers = { 1, 7 }, .favoriteNumbersCount = 2, };
+
+  printf(">>> Before copy:\n");
   Person_Print(&rogelio);
+  printf("No older Rogelio yet\n");
 
-  Person olderRogelio = Person_UpdateAge(rogelio, 34);
+  Person olderRogelio = rogelio;
+  olderRogelio.age = 34;
+  olderRogelio.name = "older Rogelio";
+  olderRogelio.favoriteNumbers[2] = 3;
+  olderRogelio.favoriteNumbersCount = 3;
+
+  printf(">>> After copy:\n");
+  Person_Print(&rogelio);
   Person_Print(&olderRogelio);
-
-  Person_Destroy(&rogelio);
-  Person_Destroy(&olderRogelio);
 
   return 0;
 }
