67fe050a99a4c566ed84baea3b1562c295dd55c2
Rogelio Gudino <cananito@gmail.com>
2023-04-18 16:09:59 -0700

    Adds exercise 1.3 of the C programming language book.

---
+++ exercises/the_c_programming_language_book/1_3_fahr_to_celsius_with_table_header/1_3_fahr_to_celsius_with_table_header.c
@@ -0,0 +1,21 @@
+// Exercise 1.3. Modify the temperature conversion program to print a heading
+// above the table.
+
+// On macOS, using clang:
+// $ clang 1_3_fahr_to_celsius_with_table_header.c
+// $ ./a.out
+
+#include <stdio.h>
+
+main() {
+  int const lower = 0;
+  int const upper = 300;
+  int const step = 20;
+  float fahr = lower;
+  printf("Fahrenheit - Celsius\n");
+  while (fahr <= upper ) {
+    float const celsius = (5.0 / 9.0) * (fahr - 32.0);
+    printf("%10.0f - %7.1f\n", fahr, celsius);
+    fahr = fahr + step;
+  }
+}
