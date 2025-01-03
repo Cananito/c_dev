1209a79335e80f9c0ddb9a0884fbbd506ebfbabd
Rogelio Gudino <cananito@gmail.com>
2025-01-03 15:03:37 -0800

    Adds a setjmp sample.

---
+++ samples/setjmp/setjmp.c
@@ -0,0 +1,19 @@
+// https://brennan.io/2020/05/24/userspace-cooperative-multitasking/
+
+#include <stdio.h>
+#include <setjmp.h>
+
+jmp_buf saved_location;
+int main(int argc, char** argv) {
+  int r = setjmp(saved_location);
+  if (r == 0) {
+    printf("We have successfully set up our jump buffer!\n");
+  } else {
+    printf("We jumped!\n");
+    return 0;
+  }
+  printf("Getting ready to jump!\n");
+  longjmp(saved_location, 1);
+  printf("This will never execute...\n");
+  return 0;
+}
