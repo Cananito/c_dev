728bdd7875aa71da3812d024b58ba0c2b737d759
Rogelio Gudino <cananito@gmail.com>
2025-03-22 09:25:12 -0700

    Adds pthread_join example.

---
+++ samples/pthread_join/pthread_join.c
@@ -0,0 +1,33 @@
+#include <pthread.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+static void* calculate(void* param) {
+  int64_t result = (int64_t)param;
+  for (size_t i = 0; i < 10000000; i++) {
+    result++;
+  }
+  return (void*)result;
+}
+
+int main(void) {
+  printf("Offloading calculation to another thread...\n");
+  pthread_t thread;
+  int create_error = pthread_create(&thread, NULL, calculate, (void*)7);
+  if (create_error != 0) {
+    printf("Couldn't create the thread. Exiting.\n");
+    exit(EXIT_FAILURE);
+  }
+
+  printf("Waiting for calculation to finish...\n");
+  void* result = 0;
+  int join_error = pthread_join(thread, &result);
+  if (create_error != 0) {
+    printf("Couldn't join the thread. Exiting.\n");
+    exit(EXIT_FAILURE);
+  }
+  printf("Calculation result: %lld\n", (int64_t)result);
+
+  return 0;
+}
