18df66ac447300bae743ed6dfcfb4ee2e12372df
Rogelio Gudino <cananito@gmail.com>
2025-09-24 19:54:13 -0700

    Adds an ncurses example.

---
+++ examples/ncurses/ncurses.c
@@ -0,0 +1,28 @@
+#include <ncurses.h>
+#include <string.h>
+
+// On macOS, using clang:
+// $ clang ncurses.c -lncurses -o ncurses
+// $ ./ncurses
+
+int main() {
+  int y;
+  int x;
+
+  WINDOW *window = initscr();
+
+  printw("Hello, ncurses!");
+
+  getmaxyx(window, y, x);
+  char* middle_message = "Middle!";
+  int middle_message_y = y / 2;
+  int middle_message_x = (x / 2) - (strlen(middle_message) / 2);
+  mvprintw(middle_message_y, middle_message_x, "%s", middle_message);
+
+  refresh();
+
+  getch();
+  endwin();
+
+  return 0;
+}
