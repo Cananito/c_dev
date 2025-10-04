2811a0a92bfbb06c9907992a7e1c58d1cc30fe9d
Rogelio Gudino <cananito@gmail.com>
2025-10-03 22:17:44 -0700

    Added overlap detection and example input.

--- exercises/interview_problems/layout_meetings/layout_meetings.c
+++ exercises/interview_problems/layout_meetings/layout_meetings.c
@@ -1,3 +1,4 @@
+#include <stdbool.h>
 #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -16,14 +17,19 @@ struct Boxes {
 };
 
 struct Event {
-  time_t const start;
-  time_t const end;
+  time_t start;
+  time_t end;
 };
 
 struct Events {
   struct Event const* const buffer;
   size_t const count;
 };
+
+static bool events_overlap(struct Event a, struct Event b) {
+  // a.start before b.end AND a.end after b.start
+  return a.start < b.end && a.end > b.start;
+}
 
 static struct Boxes event_boxes(struct Box* boxes_buffer, struct Events events) {
   // TODO: Implement algorithm!
@@ -49,6 +55,11 @@ static void print_boxes(struct Boxes boxes) {
 
 int main() {
   struct Event* events_buffer = calloc(5, sizeof(struct Event));
+  events_buffer[0] = (struct Event){ .start = 0, .end = 60 };
+  events_buffer[1] = (struct Event){ .start = 60, .end = 90 };
+  events_buffer[2] = (struct Event){ .start = 70, .end = 100 };
+  events_buffer[3] = (struct Event){ .start = 90, .end = 120 };
+  events_buffer[4] = (struct Event){ .start = 120, .end = 150 };
   struct Events events = { .buffer = events_buffer, .count = 5 };
   struct Box* boxes_buffer = calloc(5, sizeof(struct Box));
   struct Boxes boxes = event_boxes(boxes_buffer, events);
