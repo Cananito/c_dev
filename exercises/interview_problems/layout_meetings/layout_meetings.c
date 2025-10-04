8c229d0c84259618bc8a39cd10a2fb997e264c78
Rogelio Gudino <cananito@gmail.com>
2025-10-03 22:26:28 -0700

    Implemented easy invariant parts of the problem.

--- exercises/interview_problems/layout_meetings/layout_meetings.c
+++ exercises/interview_problems/layout_meetings/layout_meetings.c
@@ -31,13 +31,22 @@ static bool events_overlap(struct Event a, struct Event b) {
   return a.start < b.end && a.end > b.start;
 }
 
-static struct Boxes event_boxes(struct Box* boxes_buffer, struct Events events) {
+static struct Boxes event_boxes(struct Box* boxes_buffer, struct Events events, float container_width) {
   // TODO: Implement algorithm!
+  float earliest_start_time = 0;
+  if (events.count > 0) {
+    earliest_start_time = events.buffer[0].start;
+  }
   for (size_t i = 0; i < events.count; i++) {
-    boxes_buffer[i] = (struct Box){ .x = 0,
-                                    .y = i * 100,
-                                    .width = 100,
-                                    .height = 100 };
+    struct Event event = events.buffer[i];
+    float x = 0;
+    float y = event.start - earliest_start_time;
+    float width = container_width;
+    float height = event.end - event.start;
+    boxes_buffer[i] = (struct Box){ .x = x,
+                                    .y = y,
+                                    .width = width,
+                                    .height = height };
   }
   return (struct Boxes){ .buffer = boxes_buffer, .count = events.count };
 }
@@ -57,12 +66,12 @@ int main() {
   struct Event* events_buffer = calloc(5, sizeof(struct Event));
   events_buffer[0] = (struct Event){ .start = 0, .end = 60 };
   events_buffer[1] = (struct Event){ .start = 60, .end = 90 };
-  events_buffer[2] = (struct Event){ .start = 70, .end = 100 };
-  events_buffer[3] = (struct Event){ .start = 90, .end = 120 };
+  events_buffer[2] = (struct Event){ .start = 70, .end = 120 };
+  events_buffer[3] = (struct Event){ .start = 100, .end = 120 };
   events_buffer[4] = (struct Event){ .start = 120, .end = 150 };
   struct Events events = { .buffer = events_buffer, .count = 5 };
   struct Box* boxes_buffer = calloc(5, sizeof(struct Box));
-  struct Boxes boxes = event_boxes(boxes_buffer, events);
+  struct Boxes boxes = event_boxes(boxes_buffer, events, 100);
   print_boxes(boxes);
   free(events_buffer);
   free(boxes_buffer);
